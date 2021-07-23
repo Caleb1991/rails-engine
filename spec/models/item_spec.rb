205d290cc1a02e4613f372bb1e9ef5d444848f18
Caleb Wittman <calebjwittman1991@gmail.com>
2021-07-23 07:04:00 -0600

    Add final model test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Item do
   describe 'relationships' do
     it {should belong_to(:merchant)}
   end
+  describe 'class methods' do
+    describe 'items_displayed_per_page' do
+      it 'returns a given number of merchants with offset where applicable' do
+        merchant = create(:merchant)
+        create_list(:item, 30, merchant: merchant)
+        result = Item.items_displayed_per_page(15, 1)
+
+        expect(result.count).to eq(15)
+        expect(result.first).to eq(Item.first)
+        expect(result.last).to_not eq(Item.last)
+      end
+    end
+  end
 end
